eb9fe8f0f26dcebc56fbef77bd1197ae430c7e96
bilberats <81383654+bilberats@users.noreply.github.com>
2023-10-03 00:50:30 +0200

    réimplémentation de fullyIn
    Car les fonctions de shapely sont imprécise

--- functions.py
+++ functions.py
@@ -1,4 +1,18 @@
 from shapely import *
+import math
+
+#   detects if a polygon is fully into another
+#   returns true or false
+def fullyIn(polygon,shape):
+    #   rouding security
+    EPSILON = 0.005
+    intersection = shape.intersection(polygon)
+
+
+    if(abs(intersection.area - polygon.area) <= EPSILON):
+        return True
+    else:
+        return False
 
 #   checks if a polygon is going into that place by rotating it and moving it towards the point
 def selectPolygon(shape,point,polygons):
@@ -9,5 +23,5 @@ def selectPolygon(shape,point,polygons):
 #checks if a polygon is in the shape by moving it on the point
 def polygonIn(shape,point,polygon):
     transform(polygon.exterior.coords,lambda x: x + point)
-    return polygon.covered_by(shape)
+    return fullyIn(polygon,shape)
 
